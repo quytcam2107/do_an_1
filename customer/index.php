72c0fce95b7027a0b2f61523500a2f7bf46ea4a0
quytcam2107 <quytcam2107@github.com>
2021-01-11 16:28:17 +0700

    edit

--- customer/index.php
+++ customer/index.php
@@ -1,3 +1,13 @@
-<?php 
-	echo "Helo";
+<?php
+	$module = $action = "";
+	if (isset($_GET['module'])) {
+		$module = $_GET['module'];
+	}
+	if (isset($_GET['action'])) {
+		$action = $_GET['action'];
+	}
+	if($module == "" || $action == ""){
+		$module = "common";
+		
+	}
  ?>
